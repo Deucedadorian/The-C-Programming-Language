d28671dfc0a9fa7bf52a94170b444c4ec3b28f00
Deucedadorian <logboarddjc@gmail.com>
2023-07-23 20:07:26 -0400

    add a little exercise I did to take me through the process of compiling a c program

---
+++ compilation_tour/compiletour.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int number = 0;
+    printf("%d\n", number);
+    return 0;
+}
---
+++ compilation_tour/print1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int number = 1;
+    printf("%d\n", number);
+    return 0;
+}

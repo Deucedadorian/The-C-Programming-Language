a812e367441b3acb03d486527df52b39557a1f1e
Deucedadorian <logboarddjc@gmail.com>
2023-07-23 20:03:07 -0400

    not sure the change here but it needed to be staged...

--- chapter1/exercises/1-22.c
+++ chapter1/exercises/1-22.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 
-#define MAXCOL 50       /* length of line before line "folds" */
+#define MAXCOL 10 
 #define TABSIZE 8
-#define MAXLINE 1000
 
-int lineLength(char currentChar);
-int foldLine(char line);
+char line[MAXCOL];
+
+void printl(int pos);
 
 int main() {
-    int c;
-    char line[MAXLINE];
+    int c, pos;
+    pos = 0;
+    while((c = getchar()) != EOF) {
+        line[pos++] = c;
+        if (pos >= MAXCOL || c == '\n') {
+            printl(pos);
+            pos = 0;
+        }
+    } 
+}
 
-    while ((c = getchar()) != EOF) {
-        if (lineLength(c) > MAXCOL)
-            foldLine(line);
-    }
+void printl(int pos) {
+    int i;
+    for(i = 0; i < pos; ++i)
+        putchar(line[i]);
+    if (pos > 0)
+        putchar('\n');
 }

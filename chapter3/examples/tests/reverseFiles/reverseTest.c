bc14270e27e74fc95d245d2ebfa0389151a00f90
Deucedadorian <logboarddjc@gmail.com>
2023-07-31 18:34:09 -0400

    remove reverse function test files from it's own folder

--- chapter3/examples/tests/reverseFiles/reverseTest.c
+++
@@ -1,9 +0,0 @@
-#include "../../headers/reverse.h"
-#include <stdio.h>
-
-int main() {
-    char testString[11] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
-    printf("Test string: %s\n", testString);
-    reverse(testString);
-    printf("String after reverse: %s\n", testString);
-}

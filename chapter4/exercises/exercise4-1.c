d672aee6cd9c79ba8aa8c19db0964233e97faa99
Deucedadorian <logboarddjc@gmail.com>
2023-08-03 13:42:56 -0400

    add answer to exercise4-1 from answer book

---
+++ chapter4/exercises/exercise4-1.c
@@ -0,0 +1,13 @@
+/* strrindex: returns rightmost index of t in s, -1 if none */
+int strrindex(char s[], char t[]) {
+    int i, j, k, pos;
+
+    pos = -1;
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+            ;
+        if (k > 0 && t[k] == '\0')
+            pos = i;
+    }
+    return pos;
+}

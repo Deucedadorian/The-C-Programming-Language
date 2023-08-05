70eaf44151328fcea04c956e036ba71e339074b4
Deucedadorian <logboarddjc@gmail.com>
2023-08-04 22:05:14 -0400

    add typo fix

--- chapter4/examples/reverse_polish_calculator.c
+++ chapter4/examples/reverse_polish_calculator.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>     /* for atof()                       */
+#include <stdlib.h>     /* for atof()                               */
 
-#define MAXOP   100     /* max size of operand or operator  */
-#define NUMBER  '0'     /* signal that a number was found   */
+#define MAXOP   100     /* max size of operand or operator          */
+#define NUMBER  '0'     /* signal that a number was found           */
 
 int getop(char []);
 void push(double);
 double pop(void);
 
-/* reverse Polish calculator                                */
+/* reverse Polish calculator                                        */
 int main() {
     int type;
     double op2;
@@ -47,12 +47,12 @@ int main() {
     return 0;
 }
 
-#define MAXVAL  100     /* maximum depth of val stack   */
+#define MAXVAL  100     /* maximum depth of val stack               */
 
-int sp = 0;             /* next free stack position     */
-double val[MAXVAL];     /* value stack                  */
+int sp = 0;             /* next free stack position                 */
+double val[MAXVAL];     /* value stack                              */
 
-/* push: push f onto value stack                        */
+/* push: push f onto value stack                                    */
 void push(double f) {
     if (sp < MAXVAL)
         val[sp++] = f;
@@ -60,7 +60,7 @@ void push(double f) {
         printf("error: stack full, can't push %g\n", f);
 }
 
-/* pop: pop and return top value from stack             */
+/* pop: pop and return top value from stack                         */
 double pop(void) {
     if (sp > 0)
         return val[--sp];
@@ -70,4 +70,46 @@ double pop(void) {
     }
 }
                 
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next operator or numeric operand                      */
+int getop(char s[]) {
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;       /* not a number                             */
+    i = 0;
+    if (isdigit(c))     /* collect integer part                     */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')       /* cellect fraction part                    */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];      /* buffer for ungetch                       */
+int bufp = 0;           /* next free position in buf                */
+
+int getch(void) {       /* get a (possibly pushed back) character   */
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {   /* push character back on input             */
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else 
+        buf[bufp++] = c;
+}
 

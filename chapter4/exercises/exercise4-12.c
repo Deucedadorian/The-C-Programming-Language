21c9a258a382b7069a4cd5630e3c26abea0b0787
Deucedadorian <logboarddjc@gmail.com>
2023-09-03 15:13:48 -0400

    add answer to exercise 4-12

---
+++ chapter4/exercises/exercise4-12.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/* itoa: convert n to characters in s; recursive            */
+void itoa(int n, char s[]) {
+    static int i;
+
+    if (n / 10)
+        itoa(n / 10, s);
+    else {
+        i = 0;
+        if (n < 0)
+            s[i++] = '-';
+    }
+    s[i++]  = abs(n) % 10 + '0';
+    s[i]    = '\0';
+}
+
